b35a758743c65dee80f10a9e66e8924ca4413a6d
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-07-17 14:07:10 -0700

    Handling Arrays
    Added useState array to handle click history

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,19 +6,25 @@ const App = () => {
     right: 0,
   });
 
+  const [allClicks, setAll] = useState([]);
+
   const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
     setClicks({ ...clicks, left: clicks.left + 1 });
   };
 
   const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
     setClicks({ ...clicks, right: clicks.right + 1 });
   };
+
   return (
     <div>
       {clicks.left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {clicks.right}
+      <p>{allClicks.join(" ")}</p>
     </div>
   );
 };
