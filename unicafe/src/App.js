7323cbbbb5184959b34c993c38545cc120d5ddb6
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-07-17 14:16:55 -0700

    Conditional Rendering
    Added history and button components

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,5 +1,16 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>The app is used by pressing the buttons</div>;
+  }
+  return <div>Button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   const [clicks, setClicks] = useState({
     left: 0,
@@ -21,10 +32,10 @@ const App = () => {
   return (
     <div>
       {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
       {clicks.right}
-      <p>{allClicks.join(" ")}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 };
