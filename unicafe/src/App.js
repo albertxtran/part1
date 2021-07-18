2fe46dd5cb872f229e00c2142746fc1f8ca25f79
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-07-18 10:32:59 -0700

    Event Handling Revisited
    Used a function that returns a function to define event handler

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,41 +1,17 @@
 import { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>The app is used by pressing the buttons</div>;
-  }
-  return <div>Button press history: {props.allClicks.join(" ")}</div>;
-};
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
-
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
-
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    setClicks({ ...clicks, left: clicks.left + 1 });
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setClicks({ ...clicks, right: clicks.right + 1 });
+  const [value, setValue] = useState(10);
+  const setToValue = (number) => () => {
+    setValue(number);
   };
 
   return (
     <div>
-      {clicks.left}
-      <Button handleClick={handleLeftClick} text="left" />
-      <Button handleClick={handleRightClick} text="right" />
-      {clicks.right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
 };
