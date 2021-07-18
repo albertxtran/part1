bcc13c5b7044550e36cd14307a911eb5d26f3b99
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-07-18 10:57:09 -0700

    Do Not Define Components Within Components
    Added display component to render value

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+const Display = (props) => <div>{props.value}</div>;
+
 const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
@@ -12,7 +14,7 @@ const App = () => {
 
   return (
     <div>
-      {value}
+      <Display value={value} />
       <Button handleClick={setToValue(1000)} text="thousand" />
       <Button handleClick={setToValue(0)} text="reset" />
       <Button handleClick={setToValue(value + 1)} text="increment" />
