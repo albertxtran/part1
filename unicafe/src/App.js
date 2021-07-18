479fdd3b3ce954cb89a6a7b267e5dda031e3df1f
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-07-18 10:42:31 -0700

    Passing Event Handlers to Child Components
    Added new Button component to pass event handler to

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
 const App = () => {
   const [value, setValue] = useState(10);
   const setToValue = (number) => () => {
@@ -9,9 +13,9 @@ const App = () => {
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <Button handleClick={setToValue(1000)} text="thousand" />
+      <Button handleClick={setToValue(0)} text="reset" />
+      <Button handleClick={setToValue(value + 1)} text="increment" />
     </div>
   );
 };
