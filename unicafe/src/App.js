74e6b60cf0e123d17beaa47ebb6c3cec00bf772b
albertxtran <31325169+albertxtran@users.noreply.github.com>
2021-07-19 07:43:36 -0700

    Exercise 1.6 - 1.11
    Finished Unicafe assignment

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,23 +1,94 @@
 import { useState } from "react";
 
-const Display = (props) => <div>{props.value}</div>;
-
 const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
+const Statistics = (props) => {
+  console.log("props: ", props);
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{props.text}</td>
+          <td>{props.value}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const Display = (props) => {
+  if (
+    props.feedback.good === 0 &&
+    props.feedback.neutral === 0 &&
+    props.feedback.bad === 0
+  ) {
+    return <div>no feedback given</div>;
+  }
+
+  return (
+    <div>
+      <Statistics value={props.feedback.good} text="good" />
+      <Statistics value={props.feedback.neutral} text="neutral" />
+      <Statistics value={props.feedback.bad} text="bad" />
+      <Statistics
+        value={
+          props.feedback.good + props.feedback.neutral + props.feedback.bad
+        }
+        text="all"
+      />
+      <Statistics
+        value={
+          (props.feedback.good - props.feedback.bad) /
+          (props.feedback.good + props.feedback.neutral + props.feedback.bad)
+        }
+        text="average"
+      />
+      <Statistics
+        value={[
+          (props.feedback.good /
+            (props.feedback.good +
+              props.feedback.neutral +
+              props.feedback.bad)) *
+            100,
+          <span key="1">&#37;</span>,
+        ]}
+        text="positive"
+      />
+    </div>
+  );
+};
+
 const App = () => {
-  const [value, setValue] = useState(10);
-  const setToValue = (number) => () => {
-    setValue(number);
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const setToFeedback = (props) => {
+    if (props === "good") {
+      setFeedback({ ...feedback, good: feedback.good + 1 });
+    } else if (props === "neutral") {
+      setFeedback({ ...feedback, neutral: feedback.neutral + 1 });
+    } else {
+      setFeedback({ ...feedback, bad: feedback.bad + 1 });
+    }
   };
 
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={setToValue(1000)} text="thousand" />
-      <Button handleClick={setToValue(0)} text="reset" />
-      <Button handleClick={setToValue(value + 1)} text="increment" />
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setToFeedback("good")} text="good" />
+        <Button handleClick={() => setToFeedback("neutral")} text="neutral" />
+        <Button handleClick={() => setToFeedback("bad")} text="bad" />{" "}
+      </div>
+      <div>
+        <h1>statistics</h1>
+        <Display feedback={feedback} />
+      </div>
     </div>
   );
 };
